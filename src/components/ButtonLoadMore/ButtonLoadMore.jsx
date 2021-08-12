e8477d5a237492bda181ef5f4adffcd30cfadeb8
ilfat Aliev <ialiev685@gmail.com>
2021-08-13 00:15:31 +0300

    прикрутил нопку load more

--- src/components/ButtonLoadMore/ButtonLoadMore.jsx
+++ src/components/ButtonLoadMore/ButtonLoadMore.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './ButtonLoadMore.scss';
 
-const ButtonLoadMore = () => {
+const ButtonLoadMore = ({ onClick }) => {
   return (
-    <button className="Button" type="button">
+    <button className="Button" type="button" onClick={onClick}>
       Load more
     </button>
   );
--- src/components/GetImages/GetImages.jsx
+++ src/components/GetImages/GetImages.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import API from '../../services';
+import ButtonLoadMore from '../ButtonLoadMore/ButtonLoadMore';
 
 import ImageGallery from '../ImageGallery';
 
@@ -28,19 +29,28 @@ export default class GetImages extends Component {
     const currentPage = this.state.currentPage;
     const options = { currentPage, query };
 
-    API(options).then(resolved =>
-      this.setState(({ gallery, currentPage }) => ({
-        gallery: [...gallery, ...resolved.hits],
-        currentPage: currentPage + 1,
-      })),
-    );
+    API(options)
+      .then(resolved =>
+        this.setState(({ gallery, currentPage }) => ({
+          gallery: [...gallery, ...resolved.hits],
+          currentPage: currentPage + 1,
+        })),
+      )
+      .finally(() => {
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
   };
 
   render() {
     const { gallery } = this.state;
+
     return (
       <div>
         <ImageGallery listGallery={gallery} />
+        {gallery.length > 0 ? <ButtonLoadMore onClick={this.fetchImages} /> : null}
       </div>
     );
   }
